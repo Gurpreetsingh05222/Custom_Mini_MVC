c0b283c329579db7afe84347b281c4198f4b5983
unknown <gurpreetsingh05222@gmail.com>
2019-06-02 22:18:42 +0530

    user regis

--- models/user.php
+++ models/user.php
@@ -8,9 +8,28 @@ class UserModel extends Model{
 		$password = md5($post['password']);
 
 		if($post['submit']){
-			//Insert into db
-			$this->query('INSERT INTO users(name, email, password) VALUES(:name, :email, :password) ');
+			//SELECT * FROM users WHERE email = :email AND password = :pas
 			$this->bind(':name', $post['name']);
+			$this->bind(':email', $post['email']);
+			$this->bind(':password', $password);
+			$this->execute();
+
+			if($this->lastInsertId()){
+				header('Location: '. ROOT_URL. 'users/login');
+			}
+		}
+		return;
+	}
+
+	public function login(){
+			//Sanitize
+		$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+		$password = md5($post['password']);
+
+		if($post['submit']){
+			//Compare
+			$this->query('SELECT * FROM users WHERE email = :email AND password = :password');
 			$this->bind(':email', $post['email']);
 			$this->bind(':password', $password);
 			$this->execute();
