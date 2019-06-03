6368d06c3137a75d94396d9b26da52ff702750f1
unknown <gurpreetsingh05222@gmail.com>
2019-06-03 23:17:58 +0530

    access control for share

--- views/shares/index.php
+++ views/shares/index.php
@@ -1,6 +1,7 @@
 <div class="text-center">
+    <?php if(isset($_SESSION['is_logged_in'])) : ?>
     <a class="btn btn-success btn-share" href="<?php echo ROOT_PATH; ?>shares/add">Share something</a>
-
+    <?php endif; ?>
     <?php foreach($viewmodel as $item) : ?>
         <div class="well">
             <h3><?php echo $item['title']; ?></h3>
